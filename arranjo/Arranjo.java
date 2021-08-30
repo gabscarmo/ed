3f614a007b417815043807e5d948322c1579dbc5
Gabriela Silva Do Carmo <31321042+gabscarmo@users.noreply.github.com>
2021-08-30 13:40:00 -0300

    update Arranjo.java
    Atualização da terceira questão da lista 3

--- arranjo/Arranjo.java
+++ arranjo/Arranjo.java
@@ -24,5 +24,60 @@ public class Arranjo<T> {
         }
         System.out.println("]");
     }
+// 3.a
+    public void EscreveUltimo(T obj) {
+        for (int j = this.Data.length - 1; j >= 0; j--) {
+            if(this.Data[j]==null){
+                this.Data[j]=obj;
+                break;
+            }
+        }
+    }
+//3.b
+    public void EscrevePrimeiro(T obj) {
+        for (int j = this.Data.length - 1 ; j >= 1; j--) {
+            this.Data[j]=this.Data[j-1];
+        }
+    this.Data[0]=obj;
+    }
+//3.c
+    public void EscrevePosicao(int posicao, T obj) {
+        int x=this.Data.length - posicao - 1;
+        for (int j = 1; j < x ; j++) {
+            this.Data[posicao + j]=this.Data[posicao + j-1];        
+        }
+        this.Data[posicao] = obj;
+    }
+//3.d
+    public void RemoveUltimo() {
+        for (int j = this.Data.length-1; j > 0; j--) {
+            if(this.Data[j]!=null){
+                Get(j);
+                this.Data[j]=null;
+                break;
+            }
+        }
+    }
+//3.e
+    public void RemovePrimeiro() {    
+        for (int j = 1; j <=this.Data.length-1; j++) {
+            Get(0);
+            this.Data[j-1]=this.Data[j];
+        }
+    }
 
+//3.f
+public void EscrevePosicao(int posicao) {
+    int x=this.Data.length - posicao - 1;
+    for (int j = 1; j < x ; j++) {
+        Get(posicao);
+        this.Data[posicao + j]=this.Data[posicao + j+1];        
+    }
+}
+
+
+
+
+
+}
 }

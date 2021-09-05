8051d348c38bf1d4a95b17186b983b4e6975d736
Gabriela Silva Do Carmo <31321042+gabscarmo@users.noreply.github.com>
2021-09-05 20:24:43 -0300

    Update TestePilha.java

--- pilha/TestePilha.java
+++ pilha/TestePilha.java
@@ -2,12 +2,20 @@ package pilha;
 
 public class TestePilha {
 
-	public static void main(String[] args) {
 
-		Pilha p = new Pilha(5);
-		p.Push(10);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Pilha p = new Pilha(5);
+		try{p.Push(10);
 		p.Push(15);
-		
+        p.Push(7);
+        p.Push(3);
+        p.Push(5);
+        p.Push(1);
+        }catch (Exception e) {
+			System.out.println(e);
+		}
 		try {
 			System.out.println(p.Pop());
 			System.out.println(p.Pop());
@@ -16,6 +24,6 @@ public class TestePilha {
 			System.out.println(e);
 		}
 
-	}
+    }
 
 }

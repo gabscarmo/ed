8f8d767e17685ba92f70c41b0401ac3f87d229bc
Gabriela Silva Do Carmo <31321042+gabscarmo@users.noreply.github.com>
2021-09-05 20:23:34 -0300

    Pilha.java
    Questões 2 e 3

--- pilha/Pilha.java
+++ pilha/Pilha.java
@@ -1,40 +1,55 @@
 package pilha;
 
-public class Pilha {
-    
-    private int[] Dados;
-    public int Topo;   
+private int[] Dados;
+public int Topo;
+int Tam;   
 
-    public Pilha(int tam) {
-        this.Dados = new int[tam+1];
-        this.Topo = 0;
-    }
+public Pilha(int tam) {
+    this.Dados = new int[tam+1];
+    this.Topo = 0;
+    this.Tam = tam;
+}
 
-    public boolean StackEmpty() {
-        if (this.Topo == 0) {
-            return true;
-        } else {
-            return false;
-        }
+public boolean StackEmpty() {
+    if (this.Topo == 0) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-    public void Push(int dado) {
-        this.Topo += 1;
-        this.Dados[this.Topo] = dado;
+//Questão 2
+public boolean StackFull() {
+    if (this.Topo == this.Tam) {
+        return false;
+    } else {
+        return true;
     }
+}
 
-    public int Pop() throws Exception {
-        if (this.StackEmpty()) {
-            throw new Exception("Pilha vazia");
-        } else {
-            this.Topo -= 1;
-            return this.Dados[this.Topo+1];
-        }
-    }
 
-    public void Imprime() {
-        for (int i=0; i < this.Dados.length; i++) {
-            System.out.println(this.Dados[i]);
-        }
+//Questão 3
+public void Push(int dado) throws Exception{
+    if(this.StackFull()){
+    this.Topo += 1;
+    this.Dados[this.Topo] = dado;
+    }else{
+        throw new Exception("Pilha Cheia"); 
     }
+}
+
+public int Pop() throws Exception {
+    if (this.StackEmpty()) {
+        throw new Exception("Pilha vazia");
+    } else {
+        this.Topo -= 1;
+        return this.Dados[this.Topo+1];
+    }
+}
+
+public void Imprime() {
+    for (int i=0; i < this.Dados.length; i++) {
+        System.out.println(this.Dados[i]);
+    }
+} 
 }
